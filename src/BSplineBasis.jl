465c2f81c8e08e3d0ebb623a60e4bcc33d9ffeb4
Eivind Fonn <evfonn@gmail.com>
2014-11-24 08:08:39 +0100

    Evaluation?

--- src/BSplineBasis.jl
+++ src/BSplineBasis.jl
@@ -1,8 +1,8 @@
-type BSplineBaiss
-    knots :: Vector{Float64}
-    order :: Int
+type BSplineBasis{T}
+    knots::Vector{T}
+    order::Int
 
-    function BSplineBasis(knots, order, extend=false)
+    function BSplineBasis(knots, order, extend=true)
         for (kn, kp) in zip(knots[2:end], knots[1:end-1])
             @assert(kn >= kp, "Knot vector must be nondecreasing")
         end
@@ -13,10 +13,26 @@ type BSplineBaiss
             for d in 1:order-1
                 @assert(knots[1+d] == knots[1] && knots[end-d] == knots[end],
                         "Expected $order repeated knots on either end")
+            end
         end
 
         new(knots, order)
     end
 end
 
+uniform_basis(elements, order=4, lower=0.0, upper=1.0) =
+    BSplineBasis(linspace(lower, upper, elements+1), order)
+
 dim(knots::BSplineBasis) = length(basis.knots) - basis.order
+
+function evaluate_raw{T}(knots::Vector{T}, coeffs::Vector{T}, point::T)
+    n = length(coeffs)
+    while n > 1
+        α = (point - knots[1:n-1]) ./ (knots[end-n+2:end] - knots[1:n-1])
+        coeffs = (1 - α) .* coeffs[1:end-1] + α .* coeffs[2:end]
+        knots = knots[2:end-1]
+        n -= 1
+    end
+
+    return coeffs
+end
--- src/NURBS.jl
+++ src/NURBS.jl
@@ -1,6 +1,6 @@
 module NURBS
 
-export BSplineBasis, dim
+export BSplineBasis, dim, evaluate_raw
 
 include("BSplineBasis.jl")
 
